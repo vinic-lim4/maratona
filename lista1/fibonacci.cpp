bead02c04d41d9c259316dc7dba6327c84ad022b
vinic-lim4 <viniciuslima@estudante.ufscar.br>
2026-04-29 16:33:35 -0300

    código fibonacci

--- lista1/fibonacci.cpp
+++ lista1/fibonacci.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int T;
+    cin >> T;
+
+    unsigned long long fib[61];
+
+    fib[0] = 0;
+    fib[1] = 1;
+
+    for (int i = 2; i <= 60; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+
+    while (T--) {
+        int N;
+        cin >> N;
+        cout << "Fib(" << N << ") = " << fib[N] << endl;
+    }
+
+    return 0;
+}
